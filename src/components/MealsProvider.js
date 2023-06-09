f0336159100ac9957ca28fefc6117d4399301580
Rohit Singh Rathore <96055471+rohit511@users.noreply.github.com>
2023-06-09 11:36:56 +0530

    Update MealsProvider.js

--- src/components/MealsProvider.js
+++ src/components/MealsProvider.js
@@ -1,23 +1,79 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from 'react';
+import '../styles/App.css';
+
+const MealsContext = createContext();
+
+const App = () => {
+  return (
+    <div id="main">
+      <MealsProvider>
+        <MealsList />
+        <Counter />
+      </MealsProvider>
+    </div>
+  );
+};
+
+export default App;
+
+const Counter = () => {
+  const { meals } = useContext(MealsContext);
+  const remainingMeals = meals.filter((meal) => !meal.ticked);
+
+  return (
+    <div>
+      <h3>Meals Remaining: {remainingMeals.length}</h3>
+    </div>
+  );
+};
+
+export default Counter;
+
+const MealsList = () => {
+  const { meals, tickMeal } = useContext(MealsContext);
+
+  const handleTick = (id) => {
+    tickMeal(id);
+  };
+
+  return (
+    <div>
+      <h2>Meals:</h2>
+      <ul>
+        {meals.map((meal) => (
+          <li key={meal.id}>
+            <input type="checkbox" checked={meal.ticked} onChange={() => handleTick(meal.id)} />
+            {meal.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MealsList;
 
 const todaysMeals = [
-    { id: 1, name: 'Baked Beans' },
-    { id: 2, name: 'Baked Sweet Potatoes' },
-    { id: 3, name: 'Baked Potatoes' },
-]
+  { id: 1, name: 'Baked Beans', ticked: false },
+  { id: 2, name: 'Baked Sweet Potatoes', ticked: false },
+  { id: 3, name: 'Baked Potatoes', ticked: false },
+];
 
-const MealsProvider = () => {
-    const [meals, setMeals] = useState(todaysMeals);
+const MealsProvider = ({ children }) => {
+  const [meals, setMeals] = useState(todaysMeals);
 
-    const tickMeal = () => {
+  const tickMeal = (id) => {
+    const updatedMeals = meals.map((meal) => {
+      if (meal.id === id) {
+        return { ...meal, ticked: !meal.ticked };
+      }
+      return meal;
+    });
 
-    }
+    setMeals(updatedMeals);
+  };
 
-    return (
-        <div>
-            
-        </div>
-    )
+  return <MealsContext.Provider value={{ meals, tickMeal }}>{children}</MealsContext.Provider>;
 };
 
 export default MealsProvider;
